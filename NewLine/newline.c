1e1e660d10521cfa6e6843a3e2572e64a73e2f16
Halalisanin <Halalisanin@gmail.com>
2024-03-07 01:34:34 +0200

    variable-newline

---
+++ NewLine/newline.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return:  Always 0 (Success)
+**/
+int main(void)
+{
+printf("First line of code\n");
+printf("Second line of code\nThird line");
+return (0);
+}
---
+++ variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return:  Always 0 (Success)
+ */
+int main(void)
+{
+int number = 25;
+printf("Number is %d\n", number);
+return (0);
+}
