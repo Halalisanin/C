d0c22428f88fd475c5416a655007b8a41b6e9402
Halalisanin <Halalisanin@gmail.com>
2024-03-09 08:29:50 +0200

    character h

---
+++ C_APP/Functions/char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Descriiption:  This program demonstrtes the printing of "char"
+*
+* Retrun: Always 0 (Success)
+*/
+int main(void)
+{
+
+char c = 'h';
+printf("character: %c\n", c);
+
+return (0);
+
+}
