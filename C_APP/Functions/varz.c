b35fa29dbed3efde1275b55b75100c148885fbf2
Halalisanin <Halalisanin@gmail.com>
2024-03-09 08:45:04 +0200

    multiple variables

---
+++ C_APP/Functions/varz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints multiple variables
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+double salary = 950.5;
+int age = 25;
+
+printf("Age: %d, Salary: %f\n", age, salary);
+
+return (0);
+
+}
