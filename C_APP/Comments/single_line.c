bf151c3697acdacbc5ac5f2bd505c48b09d7c295
Halalisanin <Halalisanin@gmail.com>
2024-03-10 14:57:13 +0200

    young edit

---
+++ C_APP/Comments/single_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program shows the use of single line comments
+ *
+ * Return:  Always 0 (Success)
+ */
+int main(void)
+{
+int age;
+printf("Enter your age: ");
+
+/* take age input from the user */
+scanf("%d", &age);
+printf("Age = %d\n", age);
+
+return (0);
+
+}
---
+++ C_APP/Comments/single_two.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program shows another single line comment
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int age;
+printf("Enter your age: ");
+
+scanf("%d", &age);
+/** take age input */
+printf("Age is %d\n", age);
+/** print age */
+
+return (0);
+
+}
