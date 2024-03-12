43054a24ea9b4b35fcf9cd006faaad24b3a0fcb0
Halalisanin <Halalisanin@gmail.com>
2024-03-12 07:34:57 +0200

    long awaited update

---
+++ C_APP/Type_conversion/conversion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: This program convers data type to another
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+double x;
+x = 5;
+printf("%lf\n", x); /** 5.000000 */
+
+return (0);
+}
