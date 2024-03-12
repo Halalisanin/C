4a271538fc356254219770e61cdcd94c853564ae
Halalisanin <Halalisanin@gmail.com>
2024-03-12 07:49:27 +0200

    Int to double conversion

---
+++ C_APP/Type_conversion/doubleInt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program converts double into and int
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+double n1 = 5.4, sum;
+
+sum = 5 + n1;
+
+printf("%lf\n", sum); /** 10.400000 */
+
+return (0);
+
+}
